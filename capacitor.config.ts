ea8ac06b286dd4abf9c24147eee01c701e6281d7
Boobathi-13 <boobathirajakm.22ece@kongu.edu>
2025-09-03 10:29:59 +0530

    feat: add Capacitor setup for mobile app development

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.localbuddy.app',
+  appName: 'Local-Buddy',
+  webDir: 'dist'
+};
+
+export default config;
